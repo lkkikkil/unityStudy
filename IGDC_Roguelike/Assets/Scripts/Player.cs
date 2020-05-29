3ac48ea9c9541d2bbb5002bbe51d78313f57a6bd
seong min Kim <skattr13@naver.com>
2020-05-29 21:11:44 +0900

    Implement player move

--- IGDC_Roguelike/Assets/Scripts/Player.cs
+++ IGDC_Roguelike/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MovingObject
 {
+    public int wallDamage = 1;
     public int pointsPerFood = 10;
     public int pointsPerSoda = 20;
 
@@ -14,7 +15,10 @@ public class Player : MovingObject
     protected override void Start()
     {
         animator = GetComponent<Animator>();
+        
         food = GameManager.instance.playerFoodPoints;
+
+        base.Start();
     }
 
     private void OnDisable()
@@ -25,6 +29,36 @@ public class Player : MovingObject
     // Update is called once per frame
     void Update()
     {
-        
+        if (!GameManager.instance.playersTurn) return;
+
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0)
+            vertical = 0;
+
+        if (horizontal != 0 || vertical != 0)
+            AttemptMove<Wall>(horizontal, vertical);
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        RaycastHit2D hit;
+
+        GameManager.instance.playersTurn = false;
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger("playerChop");
     }
 }
