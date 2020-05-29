9d11716c532648add0e8b29c6876d092dd39a627
seong min Kim <skattr13@naver.com>
2020-05-29 21:23:12 +0900

    Implement next level system and interaction with other game objects

--- IGDC_Roguelike/Assets/Scripts/Player.cs
+++ IGDC_Roguelike/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MovingObject
     public int wallDamage = 1;
     public int pointsPerFood = 10;
     public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
 
     private Animator animator;
     private int food;
@@ -57,11 +58,35 @@ public class Player : MovingObject
         GameManager.instance.playersTurn = false;
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Exit")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+        else if (other.tag == "Food")
+        {
+            food += pointsPerFood;
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Soda")
+        {
+            food += pointsPerSoda;
+            other.gameObject.SetActive(false);
+        }
+    }
+
     protected override void OnCantMove<T>(T component)
     {
         Wall hitWall = component as Wall;
         hitWall.DamageWall(wallDamage);
         animator.SetTrigger("playerChop");
+    }
+
+    private void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
     }
 
     public void LoseFood (int loss)
