fa37ee6dab3607f4f792fdbb076baa4d1123f9e0
seong min Kim <skattr13@naver.com>
2020-05-30 14:36:32 +0900

    Implement object move turn system

--- IGDC_Roguelike/Assets/Scripts/GameManager.cs
+++ IGDC_Roguelike/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float turnDelay = 0.1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
     public int playerFoodPoints = 100;
@@ -11,6 +12,7 @@ public class GameManager : MonoBehaviour
 
     private int level = 32;
     private List<Enemy> enemies;
+    private bool enemiesMoving;
 
     // Start is called before the first frame update
     void Awake()
@@ -40,7 +42,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playersTurn || enemiesMoving)
+            return;
+
+        StartCoroutine(MoveEnemies());
     }
 
     public void AddEnemyToList(Enemy script)
@@ -48,4 +53,22 @@ public class GameManager : MonoBehaviour
         enemies.Add(script);
     }
 
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].movetime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
 }
