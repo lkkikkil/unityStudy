d7764c83f5b4e10693c2bafe2cb5a40946ee5498
seong min Kim <skattr13@naver.com>
2020-05-30 14:38:09 +0900

    Add enemy attack animation

--- IGDC_Roguelike/Assets/Scripts/Enemy.cs
+++ IGDC_Roguelike/Assets/Scripts/Enemy.cs
@@ -14,6 +14,7 @@ public class Enemy : MovingObject
     protected override void Start()
     {
         GameManager.instance.AddEnemyToList(this);
+        animator = GetComponent<Animator>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         base.Start();
     }
@@ -47,6 +48,8 @@ public class Enemy : MovingObject
     protected override void OnCantMove<T>(T component)
     {
         Player hitPlayer = component as Player;
+
+        animator.SetTrigger("enemyAttack");
 
         hitPlayer.LoseFood(playerDamage);
     }
