00a1b0cc4da36851d7e93863f9690f728f53e0a0
seong min Kim <skattr13@naver.com>
2020-06-06 15:45:05 +0900

    Singleton game manager

--- Poker_Battle/Assets/Scripts/GameManager.cs
+++ Poker_Battle/Assets/Scripts/GameManager.cs
@@ -5,5 +5,16 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
 }
