2e9502b9cf4ba8cafc3f640af4113acd21b6736f
seong min Kim <skattr13@naver.com>
2020-06-09 22:22:21 +0900

    Implement load ingame scene function and add to game start function

--- Poker_Battle/Assets/Scripts/GameManager.cs
+++ Poker_Battle/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -25,6 +26,11 @@ public class GameManager : MonoBehaviour
 
     public void GameStart()
     {
+        LoadInGameScene();
+    }
 
+    public void LoadInGameScene()
+    {
+        SceneManager.LoadScene("InGame");
     }
 }
